34e06ec235daa4109f2d62ba771996abf1ae81e0
Kszaboli <kovacssz@kkszki.hu>
2024-09-23 11:15:29 +0200

    Connection class is ready to use

---
+++ Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Auto
+{
+    public class Connection
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connection()
+        {
+            Host = "127.0.0.1";
+            Database = "auto";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            connection = new MySqlConnection(ConnectionString);
+        }       
+    }
+}
